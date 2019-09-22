80c4c04d8a0a253f6b0b6770a90d6419b6bea16c
luanperosa <luan.perosa@hotmail.com>
2019-09-22 11:13:33 -0300

    Complet

---
+++ exercicio.js
@@ -0,0 +1,49 @@
+function checkYear(idade) {
+  return new Promise(function(resolve, reject) {
+    if(idade >= 18) {
+      setTimeout(resolve, 2000)
+    } else {
+      setTimeout(reject, 2000)
+    }
+  });
+}
+
+checkYear()
+  .then(function(retorno) {
+    console.log('Maior que 18')
+  })
+  .catch(function() {
+    console.log('Menor de 18 anos')
+  });
+
+const inputElement = document.createElement('input');
+const buttonElement = document.createElement('button');
+const textButton = document.createTextNode('Salvar')
+
+inputElement.setAttribute('placeholder', 'Adicione o usuario do Github');
+inputElement.setAttribute('name', 'name')
+buttonElement.appendChild(textButton)
+
+const container = document.getElementById('app');
+
+container.appendChild(inputElement);
+container.appendChild(buttonElement);
+
+buttonElement.onclick = function nameDevUser() {
+  const devUser = inputElement.value;
+  document.getElementById('listRepository').innerHTML = `<h1>Carregando</h1>`
+  axios.get(`https://api.github.com/users/${devUser}/repos`)
+    .then(returned => {
+      const dataGit = returned.data
+      let html = '';
+      dataGit.map(element => {
+        html += `<li>${element.name}</li>`;
+      });
+      document.getElementById('listRepository').innerHTML = html;
+
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  
+}
